f85aa8d000e226d591daa7875508ba944c37b050
mishazawa <macintoshkrug@gmail.com>
2023-06-26 00:16:43 +0300

    rm error

--- src/main/index.cljs
+++ src/main/index.cljs
@@ -1,10 +1,8 @@
 (ns index)
 
-
 (defn log [& args]
-  (do
-    (apply prn args)
-    (apply (.-log js/console) args)))
+  (apply prn args)
+  (apply (.-log js/console) args))
 
 (println "It is works!")
 
