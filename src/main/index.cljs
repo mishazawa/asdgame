22647dc5ac857c85091e02f0d253d0891d5d16a0
mishazawa <macintoshkrug@gmail.com>
2023-06-26 03:23:17 +0300

    rm react added scene

--- src/main/index.cljs
+++ src/main/index.cljs
@@ -1,11 +1,48 @@
-(ns index)
+(ns index
+  (:require ["three" :refer [PerspectiveCamera
+                             Scene
+                             WebGLRenderer
+                             BoxGeometry
+                             Mesh
+                             MeshBasicMaterial]]
+            [utils.constants :refer [ASPECT FAR FOV H NEAR W]]))
 
-(defn log [& args]
-  (apply prn args)
-  (apply (.-log js/console) args))
+(defn Cube [args]
+  (let [[x y z] (:size args)
+        geo (new BoxGeometry x y z)
+        mat (new MeshBasicMaterial #js{:color (:color args)})]
+    (new Mesh geo mat)))
 
-(println "It is works!")
+(def rafid (atom 0))
 
-(-> (.getElementById js/document "root")
-    log)
+(defn init []
+  (let [scene (new Scene)
+        camera (new PerspectiveCamera FOV ASPECT NEAR FAR)
+        renderer (new WebGLRenderer)
+        cube (Cube {:size [1 1 1] :color 0xff0000})]
 
+    (.appendChild js/document.body renderer.domElement)
+    (.setSize renderer W H)
+    (set! camera.position.z 10)
+    (.add scene cube)
+
+    #_{:clj-kondo/ignore [:inline-def]}
+    (defn render []
+      (set! cube.rotation.x (+ cube.rotation.x 0.1))
+      (set! cube.rotation.z (+ cube.rotation.z 0.1))
+      (.render renderer scene camera))
+
+    #_{:clj-kondo/ignore [:inline-def]}
+    (defn anim []
+      (reset! rafid (.requestAnimationFrame js/window anim))
+      (render))
+
+    (anim)))
+
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn ^:dev/before-load cleanup []
+  (set! js/document.body.innerHTML "")
+  (.cancelAnimationFrame js/window @rafid))
+
+(init)
---
+++ src/main/utils/constants.cljs
@@ -0,0 +1,8 @@
+(ns utils.constants)
+
+(def W js/window.innerWidth)
+(def H js/window.innerHeight)
+(def ASPECT (/ W H))
+(def FOV 55)
+(def NEAR 1)
+(def FAR 1000)
